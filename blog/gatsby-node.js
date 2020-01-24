56b7cf4308bb3c5a75ac33dbd4f2c19011f2248d
Mitchell Smith <mitchell.smith@xero.com>
2020-01-24 14:54:04 +1300

    update route for author page

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -7,23 +7,24 @@
 const path = require(`path`);
 
 const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
-  // Query for nodes to use in creating pages.
+  // Query for article nodes to use in creating pages.
   resolve(
     graphql(request).then(result => {
       if (result.errors) {
         reject(result.errors)
       }
-      
+
       return result;
     })
   )
 });
 
+
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions;
-  
+
   const getArticles = makeRequest(graphql, `
     {
       allStrapiArticle {
@@ -46,7 +47,33 @@ exports.createPages = ({ actions, graphql }) => {
       })
     })
   });
-  
-  // Query for articles nodes to use in creating pages.
-  return getArticles;
+
+  const getAuthors = makeRequest(graphql, `
+    {
+      allStrapiUser {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+    `).then(result => {
+    // Create pages for each user.
+    result.data.allStrapiUser.edges.forEach(({ node }) => {
+      createPage({
+        path: `/authors/${node.id}`,
+        component: path.resolve(`src/templates/author.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
+  });
+
+  // Queries for articles and authors nodes to use in creating pages.
+  return Promise.all([
+    getArticles,
+    getAuthors,
+  ])
 };
